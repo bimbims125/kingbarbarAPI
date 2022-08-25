194ffca50982a1052a3e8fc029671a400a730104
bimbims125 <pramvendread@gmail.com>
2022-08-26 06:18:07 +0700

    deploy ulang

--- kingbarbarAPI/settings.py
+++ kingbarbarAPI/settings.py
@@ -122,12 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
